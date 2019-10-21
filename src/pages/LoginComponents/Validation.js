3021579d78480e52c922091c9a4a2d9c1901a6f7
Martins-O-U <martinsonyedikachi@gmail.com>
2019-10-21 14:06:48 +0100

    Validation added along with basic styles on the Login/Sign-in component

---
+++ src/pages/LoginComponents/Validation.js
@@ -0,0 +1,40 @@
+export const validationChecker = (initialState, error) => {
+    return initialState ? undefined : (error ? 'invalid' : 'valid');
+}
+
+export const validation = ({ email, password }) => {
+    return {
+        initialEmailState: (() => {
+            if(!email) return true;
+            return false;
+        })(),
+        initialPasswordState: (() => {
+            if(!password) return true;
+            return false;
+        })(),
+        emailMatch: (() => {
+            if(email)
+                return !(/\S+@\S+\.\S+/.test(email));
+        })(),
+        minMaxMatch: (() => {
+            if(password)
+                return !(password.length > 6 && password.length < 100);
+        })(),
+        numberRequired: (() => {
+            if(password)
+                return !(/[0-9]/.test(password));
+        })(),
+        lowerCaseRequired: (() => {
+            if(password)
+                return !(/[a-z]/.test(password));
+        })(),
+        upperCaseRequired: (() => {
+            if(password)
+                return !(/[A-Z]/.test(password));
+        })(),
+        specialCharRequired: (() => {
+            if(password)
+                return !(/[\!\@\#\$\%\^\&\*]/.test(password)); //eslint-disable-line
+        })()
+    }
+};
