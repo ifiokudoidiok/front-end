29679d21fb0e550631e8e8fa14802dd0bc0b0647
ofega <fegaeze@gmail.com>
2019-10-23 06:42:07 +0100

    put in data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,9 @@
 import React from "react";
 import styled from 'styled-components';
+import { slides } from '../utils/data';
 import Slider from '../components/Website/Slider';
 import Navigation from "../components/Website/Navigation";
 
-const images = [
-    "https://source.unsplash.com/1600x900/?refugees,refugee",
-    "https://source.unsplash.com/1600x900/?refugees,refugee",
-    "https://source.unsplash.com/1600x900/?refugees,refugee",
-    "https://source.unsplash.com/1600x900/?refugees,refugee",
-    "https://source.unsplash.com/1600x900/?refugees,refugee"
-];
 
 const Home = () => (
     <ParallaxContainer>
@@ -17,7 +11,7 @@ const Home = () => (
             <Navigation />
             <Slider
                 options={{
-                    autoPlay: 4000,
+                    autoPlay: 10000,
                     lazyLoad: true,
                     pauseAutoPlayOnHover: false,
                     wrapAround: true,
@@ -26,9 +20,15 @@ const Home = () => (
                 }}
             >
                 {
-                    images.map((image, index) => (
+                    slides.map(({ image, title, story }, index) => (
                         <div style={{ width: '100%', height: '100%' }} key={index}>
-                            <img src={image} data-flickity-lazyload={image} alt="" />
+                            <div className="slider-image">
+                                <img src={image} data-flickity-lazyload={image} alt="" />
+                            </div>
+                            <div className="slider-content">
+                                <h1>{title}</h1>
+                                <p>{story}</p>
+                            </div>
                         </div>
                     ))
                 }
@@ -64,6 +64,55 @@ const ParallaxContainer = styled.div`
     
             @media (max-width: 1140px) {
                 padding: 2rem;
+            }
+        }
+
+        .slider-image {
+            width: 100%;
+            height: 100%;
+
+            &:before {
+                content: "";
+                display: block;
+                position: absolute;
+                bottom: 0;
+                left: 0;
+                top: 0;
+                right: 0;
+                width: 100%;
+                height: 100%;
+                background: rgba(0, 0, 0, 0.8);
+                transition: 0.4s ease-out;
+                z-index: 2;
+            }
+        }
+
+        .slider-content {
+            position: absolute;
+            top: 0;
+            z-index: 3;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            width; 100%;
+            color: #fff;
+            padding: 2rem calc((100vw - 1140px)/2);
+    
+            @media (max-width: 1140px) {
+                padding: 2rem;
+            }
+    
+            h1 {
+                font-size: 7rem;
+                margin-bottom: 1rem;
+            }
+    
+            p {
+                font-size: 2rem;
+                max-width: 600px;
+                line-height: 1.58;
+                width: 100%;
             }
         }
     }
---
+++ src/utils/data.js
@@ -0,0 +1,28 @@
+// Slides Information
+export const slides = [
+    {
+        image: "https://images.unsplash.com/photo-1544056113-76ec529669b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+        title: "Slide 1",
+        story: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1469914449253-82de45589e53?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2133&q=80",
+        title: "Slide 2",
+        story: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1512508322418-69d6f5d2ba5c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1564&q=80",
+        title: "Slide 3",
+        story: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1566740727600-ea455147df80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1512&q=80",
+        title: "Slide 4",
+        story: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1530490125459-847a6d437825?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1526&q=80",
+        title: "Slide 5",
+        story: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+    },
+]
